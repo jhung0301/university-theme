1fb382e3c1e6f9e1b310c0ec3110f459fdfd8f96
jhung0301 <jhung0301@gmail.com>
2022-03-15 05:30:52 -0700

    Made get results Asynchronous on search.js
    Made get results Asynchronous on search.js

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '3b57ed202b0634b8d0d14fc8ccab5c21');
+<?php return array('dependencies' => array('jquery'), 'version' => 'dcd177f8c38706eb7354f3a43e708aeb');
