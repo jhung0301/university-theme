3d73eedf8dfa87d994fa126fc671d8d11cd829f3
jhung0301 <jhung0301@gmail.com>
2022-03-18 00:51:26 -0700

    Added JSON data to Search Route
    Added JSON data to Search Route

--- inc/search-route.php
+++ inc/search-route.php
@@ -9,6 +9,23 @@ function universityRegisterSearch() {
     ));
 }
 
-function universitySearchResults() {
-    return 'this is the route.';
+function universitySearchResults($data) {
+    $professors = new WP_Query(array(
+        'post_type' => 'professor',
+        's' => sanitize_text_field($data['term']) 
+    ));
+
+    $professorResults = array();
+
+    while($professors->have_posts()) {
+        $professors->the_post();
+        array_push($professorResults, array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+        ));
+    }
+
+    return $professorResults;
+
+
 }
